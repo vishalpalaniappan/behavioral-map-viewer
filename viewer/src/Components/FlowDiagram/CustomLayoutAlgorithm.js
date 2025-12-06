8bb6d98f88fdc7fa21d9562ea7a2f47182a49b6f
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-12-06 14:29:42 -0500

    Revert to previous implementation to attempt new stratergy.

--- viewer/src/Components/FlowDiagram/CustomLayoutAlgorithm.js
+++ viewer/src/Components/FlowDiagram/CustomLayoutAlgorithm.js
@@ -3,33 +3,19 @@ import sdgMeta from "../../Data/library_manager_meta.json";
 const PRINT_TO_CONSOLE = true;
 const INDENT_SPACES = 2;
 
-/**
- * Apply custom layout algorithm to visualize the
- * semantic design graph.
- * @param {Object} node
- */
 export const applyCustomLayoutAlgorithm = (node) => {
     if ("abstractions" in node) {
-        const levelWidth= visitLevel(node, 1);
-        console.log("Level Width Final:", levelWidth);
+        visitLevel(node["abstractions"], 1);
     }
-
-    console.log(node);
 };
 
-const visitLevel = (node, level) => {
-    // Get the width of the current level through how many
-    // conditional branches exist.
-    let levelWidth = node["abstractions"].length;
-    node["abstractions"].forEach((node, index) => {
+const visitLevel = (nodes, level) => {
+    nodes.forEach((node, index) => {
         printToConsole(sdgMeta[node.id], level);
-        if ("abstractions" in node && "type" in node && node["type"] === "conditional_branch") {
-            levelWidth += visitLevel(node, level + 1);
+        if ("abstractions" in node) {
+            visitLevel(node["abstractions"], level + 1);
         }
     });
-    node.level = level;
-    node.levelWidth = levelWidth - 1;
-    return levelWidth;
 };
 
 const printToConsole = (meta, level) => {
