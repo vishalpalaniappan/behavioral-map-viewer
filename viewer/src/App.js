0ae2d9771ada403e73de5075df3da48763840ad9
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-12-06 05:40:17 -0500

    Add simple layout structure.

--- viewer/src/App.js
+++ viewer/src/App.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from "react";
 
-import {FlowDiagram} from "./Components/FlowDiagram/FlowDiagram";
 import GlobalProviders from "./Providers/GlobalProviders";
+import {Viewer} from "./Viewer/Viewer";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.scss";
@@ -15,7 +15,7 @@ export function App () {
     return (
         <GlobalProviders>
             <div className="vw-100 vh-100">
-                <FlowDiagram />
+                <Viewer />
             </div>
         </GlobalProviders>
     );
